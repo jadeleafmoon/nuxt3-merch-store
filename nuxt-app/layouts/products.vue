59f63af6688c95cbabbb617c745a79dd11f58439
jadeleafmoon <jadeleafmoon@gmail.com>
2023-09-20 01:01:22 -0400

    refactor: change store name

--- nuxt-app/layouts/products.vue
+++ nuxt-app/layouts/products.vue
@@ -2,7 +2,7 @@
   <div>
     <header class="shadow-sm bg-white">
       <nav class="container mx-auto p-4">
-        <NuxtLink to="/products" class="font-bold">Nuxt Dojo Merch</NuxtLink>
+        <NuxtLink to="/products" class="font-bold">Nuxt Merch</NuxtLink>
       </nav>
     </header>
 
--- nuxt-app/pages/products/[id].vue
+++ nuxt-app/pages/products/[id].vue
@@ -12,7 +12,7 @@
   const uri = 'https://fakestoreapi.com/products/' + id
 
   // fetch the product
-  const { data: product } = await useFetch(uri)
+  const { data: product } = await useFetch(uri, { key: id})
 
   definePageMeta({
     layout: 'products'
