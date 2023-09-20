19fc0b49bf61e0b2717f9abb1d864cb817ae59b8
jadeleafmoon <jadeleafmoon@gmail.com>
2023-09-19 20:32:11 -0400

    feat: add tailwind.css

--- nuxt-app/pages/products/index.vue
+++ nuxt-app/pages/products/index.vue
@@ -1,8 +1,6 @@
 <template>
   <div>
-    <h2>Page: /products/index.vue</h2>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum ipsam mollitia dolor ut porro deleniti amet, odio doloremque commodi.</p>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad earum ipsam mollitia dolor ut porro deleniti amet, odio doloremque commodi.</p>
+    <h2>Products</h2>
   </div>
 </template>
 
